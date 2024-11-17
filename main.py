67a11625c68152b6e1409d2b3283a6c65af1a48d
yychaza14 <62446203+yychaza14@users.noreply.github.com>
2024-11-17 08:51:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -287,6 +287,7 @@ class BinanceP2PAPI:
             'Origin': 'https://p2p.binance.com',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         })
+        self._setup_directories()
 
     def search_advertisements(
         self,
